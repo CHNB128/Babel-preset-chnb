be0cc07623a91e18e2b5388fa9550c98a4ed22fd
drake.aren <drake.aren@protonmail.com>
2019-02-07 22:35:10 +0600

    add implicit-retur to plugin

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = () => ({
   presets: [require('@babel/preset-env'), require('@babel/preset-flow')],
   plugins: [
     require('babel-plugin-auto-await'),
+    require('babel-plugin-implicit-return'),
     require('babel-plugin-closure-elimination'),
     require('babel-plugin-macros'),
     require('babel-plugin-pipe-operator'),
